2f16924a7d30c70ee6f2373ec3e92ec3496bf4f4
aonemd <aonemdsaleh@gmail.com>
2016-11-27 23:31:38 +0200

    Reorganize GitDirFinderTest file
    Remove empty line
    change helper method accessor into private

--- test/git_dir_finder_test.rb
+++ test/git_dir_finder_test.rb
@@ -2,15 +2,16 @@ require 'test_helper'
 require 'changit/git_dir_finder'
 
 class GitDirFinderTest < Minitest::Test
-  def git_finder
-    Changit::GitDirFinder.new
-  end
-
   def test_find_all
     find_result = git_finder.find_all
 
     assert find_result.class == Array
     assert_equal find_result.map { |f| File.exist? f }, [true]
   end
-end
 
+  private
+
+  def git_finder
+    Changit::GitDirFinder.new
+  end
+end
