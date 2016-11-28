a48d4c6cd708df1da58814966f29f92ee182a370
aonemd <aonemdsaleh@gmail.com>
2016-11-29 01:43:59 +0200

    Implement Changit.run; system modules wrapper
    Use it inside bin/changit to run the program

--- lib/changit.rb
+++ lib/changit.rb
@@ -1,9 +1,17 @@
 require 'changit/version'
+require 'changit/git_dir_finder'
+require 'changit/config_reader'
+require 'changit/config_writer'
 
 module Changit
-  class Initial
-    def hello
-      puts 'Hello vvorld'
-    end
+  def self.run
+    # File to copy from
+    config = ConfigReader.new.read
+
+    # First, search directories inside the current pwd and make sure they contain a .git dir
+    files_to_write_to = GitDirFinder.new.find_all
+
+    # Then, copy the gitconfig file content to each .git/config file in these directories
+    ConfigWriter.new(config, files_to_write_to).write
   end
 end
