9ff13b91afd76f275ef500fed4a52e6ace23086b
aonemd <aonemdsaleh@gmail.com>
2017-01-26 21:36:48 +0200

    Pumb up to version 1.1.0

--- lib/changit/cli.rb
+++ lib/changit/cli.rb
@@ -13,22 +13,22 @@ module Changit
         opt.separator  ""
         opt.separator  "Options"
 
-        opt.on("-s","--source SOURCE","Which gitconfig file to copy from") do |src_path|
+        opt.on("-s", "--source SOURCE","Which gitconfig file to copy from") do |src_path|
           @options[:src_path] = src_path
         end
 
-        opt.on("-t","--target TARGET","Which directory to copy the config to."\
+        opt.on("-t", "--target TARGET","Which directory to copy the config to."\
                                       " To copy to different directories, pass more than one -t argument each for one directory"
               ) do |target_path|
           @options[:target_paths] << target_path
         end
 
-        opt.on("-v","--version","Show version number") do
+        opt.on("-v", "--version","Show version number") do
           puts Changit::VERSION
           exit
         end
 
-        opt.on("-h","--help","Show CLI options") do
+        opt.on("-h", "--help","Show CLI options") do
           puts opt_parser
           exit
         end
